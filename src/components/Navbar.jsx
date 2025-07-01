d343eadd57584ebf76d013c2469bd61c219b7380
Manna8928 <manali.singh.1989@gmail.com>
2025-07-01 11:54:14 +0530

    Login module

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,66 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Button from '@mui/material/Button';
 import Logo from "../assets/shoppingLogo.svg";
 import { NavLink } from 'react-router';
+import { CommerceContext } from '../context/CommerceContext';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import PowerSettingsNewIcon from '@mui/icons-material/PowerSettingsNew';
+import { googleLogout } from '@react-oauth/google';
+import { Tooltip } from '@mui/material';
 
 const Navbar = () => {
-    return (
-        <div className='flex gap-4 h-[15%] w-[100vw] justify-between items-center m-3'>
 
-            <NavLink to="/">
-                <img src={Logo} style={{ width: "4rem", marginInlineStart: "10px" }} />
-            </NavLink>
+    const { modal, setModal, token, setToken, decodedToken } = useContext(CommerceContext)
+
+    return (
+        <div className='flex h-[15%] w-[100vw] justify-between items-center m-6 p-3'>
             <div className='flex gap-10 mr-2 h-[10%] items-center justify-center'>
+                <NavLink to="/">
+                    <img src={Logo} style={{ width: "4rem", marginInlineStart: "10px" }} />
+                </NavLink>
                 <div>
                     <NavLink to="/">
-                        <Button variant="text">Home</Button>
+                        <Button variant="text" sx={{ color: "yellow" }}> Home</Button>
                     </NavLink>
-                    <Button variant="text">Shop</Button>
-                    <Button variant="text">About</Button>
-                    <Button variant="text">Contact</Button>
+                    <Button variant="text" sx={{ color: "yellow" }}>Shop</Button>
+                    <Button variant="text" sx={{ color: "yellow" }}>About</Button>
+                    <Button variant="text" sx={{ color: "yellow" }}>Contact</Button>
                 </div>
-                <Button variant="contained" sx={{ bgcolor: "red" }}>Login</Button>
+            </div>
+            <div className='logInSection'>
+                {token ?
+                    <div className='loggedIn'>
+                        {decodedToken &&
+                            <>
+                                <div className='avatar'>{decodedToken.name.substring(0, 1)}</div>
+                                <Tooltip title={decodedToken.name}>
+                                    <div className='truncatedName'>{decodedToken.name}</div>
+                                </Tooltip>
+                            </>}
+
+                        <div className='shoppingCart'>
+                            <ShoppingCartIcon sx={{ color: "white" }} fontSize='medium' />
+                            <span className='counter'>2</span>
+                        </div>
+                        <PowerSettingsNewIcon sx={{ color: "red", cursor: 'pointer' }} onClick={() => {
+                            console.log("logging out")
+                            googleLogout()
+                            setToken("")
+                        }
+                        } />
+                    </div>
+                    :
+                    <Button variant="contained" sx={{ bgcolor: "red",ml:"150px" }} onClick={() =>
+                        setModal(true)}>Login</Button>
+
+
+                }
             </div>
         </div>
+
+
+
+
     )
 }
 
